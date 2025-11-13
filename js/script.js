8b932921abcde2d48b212557a5798defb235df34
elfanmuh <elfanmuh@gmail.com>
2025-11-13 09:29:42 +0700

    Deploy Kedai Tepi

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// humberger
+
+const humberger = document.querySelector('#humberger')
+const navMenu = document.querySelector('#nav-menu')
+
+humberger.addEventListener('click', () => {
+    humberger.classList.toggle('humberger-active');
+    navMenu.classList.toggle('hidden');
+})
+
+// navbar fixed
+
+window.onscroll = () => {
+    const header = document.querySelector('header')
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed')
+    } else {
+        header.classList.remove('navbar-fixed')
+    }
+}
